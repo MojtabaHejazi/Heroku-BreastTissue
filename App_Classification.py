9cafb16a423d43429c426e8853f548ce283884c0
MojtabaHejazi <108579727+MojtabaHejazi@users.noreply.github.com>
2022-08-05 20:56:53 +0200

    Update App_Classification.py

--- App_Classification.py
+++ App_Classification.py
@@ -15,7 +15,6 @@ KNC.fit(x,y)
 pickle.dump(KNC, open('model.pkl','wb'))
 
 model=pickle.load(open('model.pkl','rb'))
-print(model.predict([290.4551412,0.144164196,0.053058009,74.63506664,1189.545213,15.93815436,35.70333099,65.54132446,330.2672929]))
 
 
 
